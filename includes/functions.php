f44fa7089c9f08a33bf2ab31ce3ec8a87d3d1719
PavitraAneja <92890507+PavitraAneja@users.noreply.github.com>
2025-08-21 23:53:23 +0530

    Matched Saved listings format to homepage
    Added the layout and filtering filtering features + the css of index.php to saved_list.php

--- includes/functions.php
+++ includes/functions.php
@@ -12,4 +12,18 @@ function build_filter_query($params) {
     }
     return $conditions ? "WHERE " . implode(" AND ", $conditions) : "";
 }
+
+function build_saved_filter_query($params) {
+    $conditions = [];
+    if (!empty($params['min'])) $conditions[] = "L_SystemPrice >= " . intval($params['min']);
+    if (!empty($params['max'])) $conditions[] = "L_SystemPrice <= " . intval($params['max']);
+    if (!empty($params['beds'])) $conditions[] = "L_Keyword2 >= " . intval($params['beds']);
+    if (!empty($params['baths'])) $conditions[] = "LM_Dec_3 >= " . intval($params['baths']);
+    if (!empty($params['city'])) $conditions[] = "L_City LIKE '%" . addslashes($params['city']) . "%'";
+    if (!empty($params['address'])) {
+        $address_escaped = addslashes($params['address']);
+        $conditions[] = "(L_City LIKE '%" . $address_escaped . "%' OR L_Zip LIKE '%" . $address_escaped . "%' OR L_Address LIKE '%" . $address_escaped . "%')";
+    }
+    return $conditions ? "AND " . implode(" AND ", $conditions) : "";
+}
 ?>
